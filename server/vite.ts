57e185ef92b88075594f6b53d83e3cf00f58798b
David Marshall <mixmediaaz@gmail.com>
2026-01-11 17:33:33 -0700

    revert 1/11/2026

--- server/vite.ts
+++ server/vite.ts
@@ -69,6 +69,7 @@ export async function setupVite(app: Express, server: Server) {
 
 export function serveStatic(app: Express) {
 <<<<<<< HEAD
+<<<<<<< HEAD
   // Try to find dist/public directory
   const possiblePaths = [
     // Vercel serverless environment
@@ -133,6 +134,10 @@ export function serveStatic(app: Express) {
 =======
   const distPath = path.resolve(import.meta.dirname, "public");
 
+=======
+  const distPath = path.resolve(import.meta.dirname, "public");
+
+>>>>>>> parent of 2e54bf0 (1/9/2026)
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -144,6 +149,9 @@ export function serveStatic(app: Express) {
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
+<<<<<<< HEAD
+>>>>>>> parent of 2e54bf0 (1/9/2026)
+=======
 >>>>>>> parent of 2e54bf0 (1/9/2026)
   });
 }
